0a629bfb9d26346f1af0f96b8b0594cc059a9a08
Arviejhay Alejandro <arviejhay123@gmail.com>
2025-08-19 16:38:05 +0800

    Adjusted sign up page

--- LogMe/Main/View/SignUpView.swift
+++ LogMe/Main/View/SignUpView.swift
@@ -14,60 +14,67 @@ struct SignUpView: View {
     var body: some View {
         ZStack {
             BackgroundView(enableAnimation: false)
-            VStack(alignment: .leading) {
-                // MARK: - Title
-                
-                Text("Create an account")
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
-                    .bold()
-                    .padding(.bottom, 30)
-                    .accessibilityAddTraits(.isHeader)
-                
-                // MARK: - Username Section
-                
-                VStack(alignment: .leading) {
-                    Text("Enter your username")
-                        .foregroundStyle(.white)
-                        .accessibilityLabel("Username section")
-                    FlexTextField(placeholder: "Username", text: $viewModel.user.username)
-                }
-                .padding(.bottom, 20)
-                .accessibilityElement(children: .contain)
-                .accessibilityLabel("Username input group")
-                
-                // MARK: - Password Section
-                
-                VStack(alignment: .leading) {
-                    Text("Enter your password")
-                        .foregroundStyle(.white)
-                        .accessibilityLabel("Password section")
-                    FlexTextField(placeholder: "Password (Min. 8 characters)", text: $viewModel.user.password, isSecured: .constant(true))
-                        .padding(.bottom, 5)
-                    FlexTextField(placeholder: "Confirm Password", text: $viewModel.confirmPassword, isSecured: .constant(true))
-                }
-                .accessibilityElement(children: .contain)
-                .accessibilityLabel("Password input group")
-                Spacer()
-                
-                // MARK: - Sign up Button
-                
-                Button {
-                    Task {
-                        await viewModel.signUp()
+            GeometryReader { geometry in
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        // MARK: - Title
+                        
+                        Text("Create an account")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                            .bold()
+                            .accessibilityAddTraits(.isHeader)
+                        
+                        Spacer()
+                        
+                        // MARK: - Username Section
+                        
+                        VStack(alignment: .leading) {
+                            Text("Enter your username")
+                                .foregroundStyle(.white)
+                                .accessibilityLabel("Username section")
+                            FlexTextField(placeholder: "Username", text: $viewModel.user.username)
+                        }
+                        .padding(.bottom, 20)
+                        .accessibilityElement(children: .contain)
+                        .accessibilityLabel("Username input group")
+                        
+                        // MARK: - Password Section
+                        
+                        VStack(alignment: .leading) {
+                            Text("Enter your password")
+                                .foregroundStyle(.white)
+                                .accessibilityLabel("Password section")
+                            FlexTextField(placeholder: "Password (Min. 8 characters)", text: $viewModel.user.password, isSecured: .constant(true))
+                                .padding(.bottom, 5)
+                            FlexTextField(placeholder: "Confirm Password", text: $viewModel.confirmPassword, isSecured: .constant(true))
+                        }
+                        .accessibilityElement(children: .contain)
+                        .accessibilityLabel("Password input group")
+                        Spacer()
+                        
+                        // MARK: - Sign up Button
+                        
+                        Button {
+                            Task {
+                                await viewModel.signUp()
+                            }
+                        } label: {
+                            Text("Sign up")
+                                .padding(8)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.roundedRectangle)
+                        .shadow(color: .black.opacity(0.3), radius: 5)
+                        .accessibilityHint("Creates your account with the entered information.")
                     }
-                } label: {
-                    Text("Sign up")
-                        .padding(8)
-                        .frame(maxWidth: .infinity, alignment: .center)
+                    .padding()
+                    .disabled(viewModel.isLoading)
+                    .frame(width: geometry.size.width, height: geometry.size.height)
                 }
-                .buttonStyle(.borderedProminent)
-                .buttonBorderShape(.roundedRectangle)
-                .shadow(color: .black.opacity(0.3), radius: 5)
-                .accessibilityHint("Creates your account with the entered information.")
+                .scrollBounceBehavior(.basedOnSize)
             }
-            .padding()
-            .disabled(viewModel.isLoading)
             
             if viewModel.isLoading {
                 LoadingView()
